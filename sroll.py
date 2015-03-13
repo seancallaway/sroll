f6cb5bd474a0040334fe23f8cf19740f7099799c
Sean Callaway <sean@chewie.hq.calnet.us>
2015-03-13 09:12:37 -0700

    Added die rolling functionality.

--- sroll.py
+++ sroll.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python
 
-import sys, getopt
+import sys, getopt, random
 
 def main(argv):
 	rawdice = ''
 	numdice = 0
 	diesize = 0
 	modifier = 0
+	result = 0
 	
 	numargs = len(sys.argv)
 	
@@ -17,12 +18,28 @@ def main(argv):
 		modifier = sys.argv[2]
 	rawdice = sys.argv[1]
 	
+	random.seed()
+	
+	#############################
+	# TODO: Sanitize user input #
+	#############################
+	
 	numdice, diesize = rawdice.split('d', 1)
 	
-	print 'Number of dice: ', numdice
-	print 'Die size: ', diesize
-	if modifier != 0:
-		print 'Modified: ', modifier
+	# Debugging bit to verify input
+	#print 'Number of dice: ', numdice
+	#print 'Die size: ', diesize
+	#if modifier != 0:
+	#	print 'Modifier: ', modifier
+		
+	# Roll the dice
+	for i in range(int(numdice)):
+		rolled = random.randint(1, int(diesize))
+		result += rolled
+	
+	result += int(modifier)
+	
+	print "Rolling", rawdice, "plus", modifier, ": ", result
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
